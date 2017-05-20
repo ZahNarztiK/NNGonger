3bac45611ad865b5355f54cd890a8f4ef2e575f8
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-05-21 06:54:04 +0700

    Conseal, TrimReward
    - Version changed to 1.0
    - No data = N/A
    - Add consealing data to ' '
    - Add trimming result

--- nngg.js
+++ nngg.js
@@ -1,14 +1,13 @@
 function nn_inj(){
-	var ht=$('html').html()
+	var ht=$('html').html(),
 		cn=$('#ComNumCheck').val(),
-		mn=/txtMacAddress = .*;/i.exec(ht);
-	if(cn&&mn){
-		mn=/.*['"](.*)['"]/.exec(mn)[1];
-		var rs=prompt(`### Start Gonging ###\nComNo: ${cn}\nMacAddress: ${mn}`);
-		if(rs||isNaN(rs)){
-			rs=Number(rs).toFixed(2);
-			$.post('RunSearchReward.php',{RewardValue:rs,ComNo:cn,MacAddress:mn},data=>alert(data));
-		}
+		mn=/txtMacAddress = ['"](.*)['"];/i.exec(ht)[1];
+	var rw=prompt(`### Start Gonging ###\nComNo: ${cn=(cn?cn:"N/A")}\nMacAddress: ${mn=(mn?mn:"N/A")}`);
+	if(rw&&!isNaN(rw)){
+		rs=confirm(`### Consealed Protocol ###\nComNo: ${cn}\nMacAddress: ${mn}\n\nDo you want to conseal them?\n(OK: Mask with ' ', Cancel: Leave it)`);
+		$.post('RunSearchReward.php',{RewardValue:Number(rw).toFixed(2),ComNo:(rs?' ':cn),MacAddress:(rs?' ':mn)},data=>{
+			var spl=/: (.*) <br>Password: (.*)</i.exec(data);
+			alert(spl?`${spl[1]}\n${spl[2]}`:data);
+		});
 	}
-	else alert("Gonging is unavailable!\nLack of:\n"+(cn?"- ComNo\n":"")+(mn?"- MacAddress\n":""));
 }
